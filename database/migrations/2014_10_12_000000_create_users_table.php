412cf5c94ba7dd91b577f8f3aa99986f1dd86570
cvelasquez <carlosrobertovelasquez@gmail.com>
2019-09-02 23:32:29 -0600

    Creacion de tablas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('role_id')->unsigned();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('active');
             $table->timestamps();
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
